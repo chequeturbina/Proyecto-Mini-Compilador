666c6d435a1db759f535c63306a10eb43fd619b0
juan121694 <juan121694@gmail.com>
2019-12-28 18:24:06 -0600

    pilas y tablas

--- tablaTipo.c
+++ tablaTipo.c
@@ -30,27 +30,112 @@ struct _typetab{
 };
 
 /*Retorna un apuntador a una variable type*/
-type *crearTipo();
+type *crearTipo(){
+	type *new = malloc(sizeof(type));
+	new->id=0;
+	new->nombre="";
+	new->tipoBase=NULL;
+	new->tamBytes=0;
+	new->numElem=0;
+	new->next=NULL;
+	return new;
+}
 
 /*Borra type, libera la memoria*/
-void borraType(type *t);
+void borraType(type *t){
+	free(t);
+}
 
 /*inserta al final de la lista en caso de insertar incrementa num
  *retorna la posicion donde inserto en caso contrario retorna -1*/
- int insertarTipo(typetab *tt, type *t);
+ int insertarTipo(typetab *tt, type *t){
+	type *iter = tt->root;
+	for (int i = 0; i < tt->num; ++i)
+	{	
+		if(num==0){
+			tt->root=t;
+			num=num+1;
+			return num;
+		}
+		if(t->id==iter->id){
+			return -1;
+		}
+		if(iter->next==NULL){
+			iter->next=t;
+			num= num+1;
+			return num;
+		}
+	}
+}
 
  /*Retorna el tipo base de un tipo
   *En caso de no encontrarlo retorna NULL*/
- TipoBase getTipoBase(typetab *tt, int id);
+ TipoBase getTipoBase(typetab *tt, int id){
+	type *iter = tt->root;
+	for (int i = 0; i < tt->num; ++i)
+	{	
+		if(num==0){
+			return -1;
+		}
+		if(id==iter->id){
+			return iter->tipoBase;
+		}
+		if(iter->next==NULL){
+			return -1;
+		}
+	}
+}
 
  /*Retorna el numero de bytes de un tipo
   *En caso de no encontrarlo retorna -1*/
- int getTam(typetab *tt, int id);
+ int getTam(typetab *tt, int id){
+	type *iter = tt->root;
+	for (int i = 0; i < tt->num; ++i)
+	{	
+		if(num==0){
+			return -1;
+		}
+		if(id==iter->id){
+			return iter->tamBytes;
+		}
+		if(iter->next==NULL){
+			return -1;
+		}
+	}
+ }
 
  /*Retorna el numero de elementos de un tipo
   *En caso de no encontrarlo retorna -1*/
- int getNumElem(typetab *tt, int id);
+ int getNumElem(typetab *tt, int id){
+	type *iter = tt->root;
+	for (int i = 0; i < tt->num; ++i)
+	{	
+		if(num==0){
+			return -1;
+		}
+		if(id==iter->id){
+			return iter->numElem;
+		}
+		if(iter->next==NULL){
+			return -1;
+		}
+	}
+}
 
  /*Retorna el nombre de un tipo 
   *En caso de no encontrarlo retorna NULL*/
- char* getNombre(typetab *tt, int id);
+ char* getNombre(typetab *tt, int id){
+	type *iter = tt->root;
+	for (int i = 0; i < tt->num; ++i)
+	{	
+		if(num==0){
+			return NULL;
+		}
+		if(id==iter->id){
+			return iter->nombre;
+		}
+		if(iter->next==NULL){
+			return NULL;
+		}
+	}
+}
